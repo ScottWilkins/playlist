6ed0f2a13563ba5f96f2c9ff2e7831dafe733d35
Scott Wilkins <scottwilkins@MacBook-Air.local>
2016-06-09 11:18:38 -0600

    spotify refactor

--- scripts/script2.js
+++ scripts/script2.js
@@ -1,46 +1,53 @@
 $(document).ready(function(){
-  var $tracks    = $('#track_div'),
-      button     = $('#button');
+
+  var button = $('#button');
   $('button').on('click', searchArtist);
   $(document).on('click','.artists', getAlbums)
   $(document).on('click','.tracks', showTracks)
 
   function searchArtist(artist){
       artist = $('#title').val();
-      callAjax("https://api.spotify.com/v1/search?q=", artist, showNames, '&type=artist', 'artists')
+      callAjax("search?q=", artist, showNames, '&type=artist', 'artists')
     }
+
   function showNames(names){
+       var $artists     = $('#artists')
+       $artists.empty()
        names.forEach(name => {
              var artURI = name.id
              var p      = document.createElement("p")
              p.id       = artURI;
              $(p).addClass("artists");
              $(p).html(name.name)
-             $('#artists').append(p)
+             $artists.append(p)
        })
   }
+
   function getAlbums(){
-      callAjax("https://api.spotify.com/v1/artists/", $(this).context.id, showAlbums, '/albums')
+      callAjax("artists/", $(this).context.id, showAlbums, '/albums')
   }
+
   function showAlbums(albums){
-        var nameArray = []
+      var $tracks     = $('#track_div')
+      var nameArray   = []
+      $tracks.empty()
       albums.forEach(album => {
         var name  = album.name;
         var id    = album.id;
         var image = album.images[1].url;
         if(nameArray.indexOf(name.toLowerCase()) === -1){
-                   var inpt = document.createElement('input');
-                   inpt.type = 'button';
-                   $(inpt).css("background-image",`url(${image})`)
-                   $(inpt).addClass('tracks')
-                   $(inpt).attr('id', id)
+                   var input                   = document.createElement('input');
+                   input.type                  = 'button';
+                   input.style.backgroundImage = `url(${image})`
+                   $(input).addClass('tracks')
+                   $(input).attr('id', id)
                    nameArray.push(name.toLowerCase())
-                   $tracks.append(inpt)
+                   $tracks.append(input)
                   }
             })
   }
   function showTracks(){
-    callAjax("https://api.spotify.com/v1/albums/", this.id, populatePlayerArr, '/tracks')
+    callAjax("albums/", this.id, populatePlayerArr, '/tracks')
 
   }
   function populatePlayerArr(tracks){
@@ -48,17 +55,18 @@ $(document).ready(function(){
     tracks.forEach(item => {
          TrackArr.push(item.id)
      })
-          populatePlayer(TrackArr)
+    populatePlayer(TrackArr)
     }
+
   function populatePlayer(arr){
     var list = arr.join(",");
     $("iframe").attr("src",`https://embed.spotify.com/?uri=spotify:trackset:PREFEREDTITLE:${list}`)
   }
 
-  function callAjax(url,variable,func,optionUrl,resultKey){
+  function callAjax(url, variable, func, optionUrl, resultKey){
     optionUrl = optionUrl || ""
     $.ajax({
-      url: url + variable + optionUrl}).done(function (results) {
+      url: "https://api.spotify.com/v1/" + url + variable + optionUrl}).done(function (results) {
       resultKey ? func(results[resultKey].items) : func(results.items)
    })
   }
