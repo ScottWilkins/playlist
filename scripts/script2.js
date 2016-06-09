2af1482de20ff27ea407afff19e52518a0c40c6d
Scott Wilkins <scottwilkins@MacBook-Air.local>
2016-06-08 23:01:06 -0600

    refactor code

--- scripts/script2.js
+++ scripts/script2.js
@@ -1,54 +1,65 @@
 $(document).ready(function(){
   var $tracks    = $('#track_div'),
-      $bin       = $('#center'),
       button     = $('#button');
-button.on('click', function(){
-  $('#artists').html('')
-  var artist = $('#title').val();
+  $('button').on('click', searchArtist);
+  $(document).on('click','.artists', getAlbums)
+  $(document).on('click','.tracks', showTracks)
 
-  $.get(`https://api.spotify.com/v1/search?q=${artist}&type=artist`, function(data){
-    $.each(data,(function(idx, item){
-      item.items.forEach(artists => {
-        var artURI = artists.uri.slice(15);
-        $('#artists').append(`<p id=${artURI} class="artists">${artists.name}</p>`)
+  function searchArtist(artist){
+      artist = $('#title').val();
+      callAjax("https://api.spotify.com/v1/search?q=", artist, showNames, '&type=artist', 'artists')
+    }
+  function showNames(names){
+       names.forEach(name => {
+             var artURI = name.id
+             var p      = document.createElement("p")
+             p.id       = artURI;
+             $(p).addClass("artists");
+             $(p).html(name.name)
+             $('#artists').append(p)
+       })
+  }
+  function getAlbums(){
+      callAjax("https://api.spotify.com/v1/artists/", $(this).context.id, showAlbums, '/albums')
+  }
+  function showAlbums(albums){
+        var nameArray = []
+      albums.forEach(album => {
+        var name  = album.name;
+        var id    = album.id;
+        var image = album.images[1].url;
+        if(nameArray.indexOf(name.toLowerCase()) === -1){
+                   var inpt = document.createElement('input');
+                   inpt.type = 'button';
+                   $(inpt).css("background-image",`url(${image})`)
+                   $(inpt).addClass('tracks')
+                   $(inpt).attr('id', id)
+                   nameArray.push(name.toLowerCase())
+                   $tracks.append(inpt)
+                  }
+            })
+  }
+  function showTracks(){
+    callAjax("https://api.spotify.com/v1/albums/", this.id, populatePlayerArr, '/tracks')
 
-      });
-    }))
-  $('.artists').on("click", function(){
-    $tracks.html("");
-    var nameArray = [];
-    $.get(`https://api.spotify.com/v1/artists/${$(this).context.id}/albums`, function(data){
-      data.items.forEach(alb => {
-           if(nameArray.indexOf(alb.name.toLowerCase()) === -1){
-           var $input   = $('<input>');
-           $input.css('background-image', `url(${alb.images[1].url})`).
-                  attr('type', 'button').
-                  addClass('tracks').
-                  attr('id', alb.id);
-                  nameArray.push(alb.name.toLowerCase())
-           $tracks.append($input)
-        }
+  }
+  function populatePlayerArr(tracks){
+    var TrackArr = [];
+    tracks.forEach(item => {
+         TrackArr.push(item.id)
+     })
+          populatePlayer(TrackArr)
+    }
+  function populatePlayer(arr){
+    var list = arr.join(",");
+    $("iframe").attr("src",`https://embed.spotify.com/?uri=spotify:trackset:PREFEREDTITLE:${list}`)
+  }
 
-      })
-
-         //send button info to bin
-          $('.tracks').on('click',function(){
-              $bin.html("")
-              data.items.forEach(item => {
-                var TrackArr = [];
-                  if(item.id == $(this)[0].id){
-                      $.get(`https://api.spotify.com/v1/albums/${item.id}/tracks`, function(trax){
-                      trax.items.forEach(Track => {
-                      TrackArr.push(Track.id)
-                      });
-                      var TrackId = TrackArr.join(",")
-                      $("iframe").attr("src",`https://embed.spotify.com/?uri=spotify:trackset:PREFEREDTITLE:${TrackArr}`)
-                         })
-                       }
-                    })
-                  })
-               })
-             })
-           })
-         })
+  function callAjax(url,variable,func,optionUrl,resultKey){
+    optionUrl = optionUrl || ""
+    $.ajax({
+      url: url + variable + optionUrl}).done(function (results) {
+      resultKey ? func(results[resultKey].items) : func(results.items)
+   })
+  }
 })
